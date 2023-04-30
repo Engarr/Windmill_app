74548c3a38cb666df4c072ae4aa9b4e49451c9a6
Łukasz <lukasz.poczta12@gmail.com>
2023-04-30 15:48:34 +0200

    changed backend configuration

---
+++ api/controllers/auth.js
@@ -0,0 +1,4 @@
+export const signup = async (req, res, next) => {
+  const name = req.body.name;
+  const password = req.body.password;
+};
--- api/index.js
+++ api/index.js
@@ -1,18 +1,43 @@
 import express from 'express';
 const app = express();
-import dotenv from 'dotenv';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
 dotenv.config();
 
+import authRouter from './routers/auth.js';
+
 app.use(cors());
-app.get('/api/test', (req, res) => {
-  const time = new Date();
-  res.json('hello world 2 ' + time);
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, X-User-Id'
+  );
+  next();
 });
 
+app.use('/auth', authRouter);
+
 if (process.env.VITE_API_PORT) {
-  app.listen(process.env.VITE_API_PORT);
-  console.log('server running');
+  mongoose
+    .connect(
+      `mongodb+srv://Lukasz:${process.env.VITE_API_MOONGOSE_PASS}@cluster0.k4a8s6m.mongodb.net/ecommers?retryWrites=true`
+    )
+    .then((result) => {
+      app.listen(process.env.VITE_API_PORT);
+      console.log('server is running');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 export default app;
