cfe007cc95ecdb0c8996aa5afe70f5892e90dfc5
Łukasz <lukasz.poczta12@gmail.com>
2023-05-01 09:01:45 +0200

    conecting app to vercel

--- api/index.js
+++ api/index.js
@@ -33,18 +33,16 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, data: data });
 });
 
-if (process.env.VITE_API_PORT) {
-  mongoose
-    .connect(
-      `mongodb+srv://Lukasz:${process.env.VITE_API_MOONGOSE_PASS}@cluster0.k4a8s6m.mongodb.net/windmil?retryWrites=true`
-    )
-    .then((result) => {
-      app.listen(process.env.VITE_API_PORT);
-      console.log('server is running');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+mongoose
+  .connect(
+    `mongodb+srv://Lukasz:${process.env.VITE_API_MOONGOSE_PASS}@cluster0.k4a8s6m.mongodb.net/windmil?retryWrites=true`
+  )
+  .then((result) => {
+    app.listen(process.env.VITE_API_PORT);
+    console.log('server is running');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 export default app;
