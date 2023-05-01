07d901636820783ee16caa64224f31683fe33dd3
Łukasz <lukasz.poczta12@gmail.com>
2023-05-01 10:28:08 +0200

    test

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -2,25 +2,26 @@ import bcrypt from 'bcrypt';
 import User from '../models/user.js';
 
 export const signup = async (req, res, next) => {
-    
-  const email = req.body.email;
-  const password = req.body.password;
- 
-  try {
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = new User({
-      email: email,
-      password: hashedPassword,
-    });
-    const result = await user.save();
-    res
-      .status(201)
-      .json({ message: 'User has been created', userId: result._id });
-  } catch (err) {
-    if (!err) {
-      err.statusCode = 500;
-      err.message = 'something went wrong';
-    }
-    next(err);
-  }
+  const date = new Date().getFullYear();
+  res.send(date.toString());
+  // const email = req.body.email;
+  // const password = req.body.password;
+
+  // try {
+  //   const hashedPassword = await bcrypt.hash(password, 12);
+  //   const user = new User({
+  //     email: email,
+  //     password: hashedPassword,
+  //   });
+  //   const result = await user.save();
+  //   res
+  //     .status(201)
+  //     .json({ message: 'User has been created', userId: result._id });
+  // } catch (err) {
+  //   if (!err) {
+  //     err.statusCode = 500;
+  //     err.message = 'something went wrong';
+  //   }
+  //   next(err);
+  // }
 };
