7bb5bf234c8750eaf2906b79ed1b418e0caa3429
Łukasz <lukasz.poczta12@gmail.com>
2023-05-01 10:54:27 +0200

    test contection to vercel-

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -2,26 +2,25 @@ import bcrypt from 'bcrypt';
 import User from '../models/user.js';
 
 export const signup = async (req, res, next) => {
-  const date = new Date().getFullYear();
-  res.send(date.toString());
-  // const email = req.body.email;
-  // const password = req.body.password;
+ 
+  const email = req.body.email;
+  const password = req.body.password;
 
-  // try {
-  //   const hashedPassword = await bcrypt.hash(password, 12);
-  //   const user = new User({
-  //     email: email,
-  //     password: hashedPassword,
-  //   });
-  //   const result = await user.save();
-  //   res
-  //     .status(201)
-  //     .json({ message: 'User has been created', userId: result._id });
-  // } catch (err) {
-  //   if (!err) {
-  //     err.statusCode = 500;
-  //     err.message = 'something went wrong';
-  //   }
-  //   next(err);
-  // }
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+    });
+    const result = await user.save();
+    res
+      .status(201)
+      .json({ message: 'User has been created', userId: result._id });
+  } catch (err) {
+    if (!err) {
+      err.statusCode = 500;
+      err.message = 'something went wrong';
+    }
+    next(err);
+  }
 };
