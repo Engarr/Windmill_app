cf52a01018a6df5ad6959ff5bb10b433a09c8bcf
Łukasz <lukasz.poczta12@gmail.com>
2023-05-21 14:00:15 +0200

    changed ProductDetail component

--- src/components/CartProduct/CartProduct.tsx
+++ src/components/CartProduct/CartProduct.tsx
@@ -25,70 +25,75 @@ const CartProduct = (props: { products: Product[]; token: string }) => {
       toast.error('Wystąpił błąd podczas dodawania produktu do koszyka.');
     }
   };
-
   return (
     <div className={classes.container}>
-      <table>
-        <thead>
-          <th></th>
-          <th>Nazwa</th>
-          <th>Cena</th>
-          <th>Ilość</th>
-          <th>Suma</th>
-          <th></th>
-        </thead>
-        <tbody>
-          {cartProduct.map((product) => (
-            <tr key={product.product._id}>
-              <td>
-                <div className={classes.container__imgBox}>
-                  <img
-                    src={product.product.imageUrl}
-                    width={120}
-                    alt={product.product.name}
-                  />
-                </div>
-              </td>
-              <td data-cell="Nazwa:">
-                <div className={classes.container__nameBox}>
-                  <Link to={`/produkt/${product.product._id}`}>
-                    <p>{product.product.name}</p>
-                  </Link>
-                </div>
-              </td>
-              <td data-cell="Cena:">
-                <div className={classes.container__priceBox}>
-                  <p>{product.product.price} zł</p>
-                </div>
-              </td>
-              <td data-cell="Ilość:">
-                <div className={classes.container__qtyBox}>
-                  <button>+</button>
-                  <p> {product.quantity}</p>
-                  <button>-</button>
-                </div>
-              </td>
-              <td data-cell="Suma:">
-                <div className={classes.container__sumBox}>
-                  <p>{product.quantity * product.product.price} zŁ</p>
-                </div>
-              </td>
-              <td>
-                <div
-                  className={classes.container__deleteBox}
-                  onClick={() => {
-                    removeProduct(product.product._id, product.product.name);
-                  }}
-                >
-                  <RiDeleteBinLine
-                    className={classes[`container__deleteBox--icon`]}
-                  />
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {cartProduct.length <= 0 ? (
+        <div className={classes.emptyCart}>
+          <h2>Twój koszyk jest pusty</h2>
+          <button>Wróć do sklepu</button>
+        </div>
+      ) : (
+        <table>
+          <tbody>
+            <th></th>
+            <th>Nazwa</th>
+            <th>Cena</th>
+            <th>Ilość</th>
+            <th>Suma</th>
+            <th></th>
+
+            {cartProduct.map((product) => (
+              <tr key={product.product._id}>
+                <td>
+                  <div className={classes.container__imgBox}>
+                    <img
+                      src={product.product.imageUrl}
+                      width={120}
+                      alt={product.product.name}
+                    />
+                  </div>
+                </td>
+                <td data-cell="Nazwa:">
+                  <div className={classes.container__nameBox}>
+                    <Link to={`/produkt/${product.product._id}`}>
+                      <p>{product.product.name}</p>
+                    </Link>
+                  </div>
+                </td>
+                <td data-cell="Cena:">
+                  <div className={classes.container__priceBox}>
+                    <p>{product.product.price} zł</p>
+                  </div>
+                </td>
+                <td data-cell="Ilość:">
+                  <div className={classes.container__qtyBox}>
+                    <button>+</button>
+                    <p> {product.quantity}</p>
+                    <button>-</button>
+                  </div>
+                </td>
+                <td data-cell="Suma:">
+                  <div className={classes.container__sumBox}>
+                    <p>{product.quantity * product.product.price} zŁ</p>
+                  </div>
+                </td>
+                <td>
+                  <div
+                    className={classes.container__deleteBox}
+                    onClick={() => {
+                      removeProduct(product.product._id, product.product.name);
+                    }}
+                  >
+                    <RiDeleteBinLine
+                      className={classes[`container__deleteBox--icon`]}
+                    />
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
