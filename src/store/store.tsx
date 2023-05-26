f6fdc835a6402c99cbc444383330331e795981b2
Łukasz <lukasz.poczta12@gmail.com>
2023-05-26 11:38:51 +0200

    connecting test

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -2,13 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import uiReducer from './ui-slice';
 // import { productsApi } from './api/apiSlice';
 import apiSlice from './api/apiSlice';
-// import cartItemsReducer from './cartSlice';
+import cartItemsReducer from './cartSlice';
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     ui: uiReducer,
-    // cartItems: cartItemsReducer,
+    cartItems: cartItemsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
