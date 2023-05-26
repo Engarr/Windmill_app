11c9475b29bde275622bae604cb6b5e4b5e2b5ef
Łukasz <lukasz.poczta12@gmail.com>
2023-05-26 11:29:22 +0200

    test

--- src/components/Cart/NotRegisteredCart/NotRegisteredCart.tsx
+++ src/components/Cart/NotRegisteredCart/NotRegisteredCart.tsx
@@ -1,75 +1,75 @@
-import { useMemo } from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { cartItemAction } from '../../../store/cartSlice';
-import { useGetProductsByIdQuery } from '../../../store/api/productsApiSlice';
-import Spinner from '../../Spinner/Spinner/Spinner';
-import CartProduct from '../../CartProduct/CartProduct';
-import classes from '../../../pages/Cart/Cart.module.scss';
-import { RootState } from '../../../store/store';
-import { ProductType } from '../../../types/types';
+// import { useMemo } from 'react';
+// import { Link } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { cartItemAction } from '../../../store/cartSlice';
+// import { useGetProductsByIdQuery } from '../../../store/api/productsApiSlice';
+// import Spinner from '../../Spinner/Spinner/Spinner';
+// import CartProduct from '../../CartProduct/CartProduct';
+// import classes from '../../../pages/Cart/Cart.module.scss';
+// import { RootState } from '../../../store/store';
+// import { ProductType } from '../../../types/types';
 
-interface StorageItemsArrType {
-  data: {
-    products: ProductType[];
-  };
-  isLoading: boolean;
-}
+// interface StorageItemsArrType {
+//   data: {
+//     products: ProductType[];
+//   };
+//   isLoading: boolean;
+// }
 
-const NotRegisteredCart = () => {
-  const dispatch = useDispatch();
-  const storageItems = useSelector((state: RootState) => state.cartItems.items);
-  const idArr = storageItems.map(({ productId }) => productId);
+// const NotRegisteredCart = () => {
+//   const dispatch = useDispatch();
+//   const storageItems = useSelector((state: RootState) => state.cartItems.items);
+//   const idArr = storageItems.map(({ productId }) => productId);
 
-  const { data: storageItemsArr, isLoading } =
-    useGetProductsByIdQuery<StorageItemsArrType>(
-      idArr.length > 0 ? idArr : undefined
-    );
+//   const { data: storageItemsArr, isLoading } =
+//     useGetProductsByIdQuery<StorageItemsArrType>(
+//       idArr.length > 0 ? idArr : undefined
+//     );
 
-  const newArr = useMemo(() => {
-    if (storageItems && storageItemsArr?.products) {
-      return storageItems.map((item) => {
-        const product = storageItemsArr.products.find(
-          (p) => p._id === item.productId
-        );
-        return {
-          product,
-          quantity: item.quantity,
-        };
-      });
-    }
-    return [];
-  }, [storageItems, storageItemsArr]);
+//   const newArr = useMemo(() => {
+//     if (storageItems && storageItemsArr?.products) {
+//       return storageItems.map((item) => {
+//         const product = storageItemsArr.products.find(
+//           (p) => p._id === item.productId
+//         );
+//         return {
+//           product,
+//           quantity: item.quantity,
+//         };
+//       });
+//     }
+//     return [];
+//   }, [storageItems, storageItemsArr]);
 
-  const increaseHandler = (id: string) => {
-    dispatch(cartItemAction.onIncreaseQty(id));
-  };
-  const decreaseHandler = (id: string) => {
-    dispatch(cartItemAction.onDecreaseQty(id));
-  };
+//   const increaseHandler = (id: string) => {
+//     dispatch(cartItemAction.onIncreaseQty(id));
+//   };
+//   const decreaseHandler = (id: string) => {
+//     dispatch(cartItemAction.onDecreaseQty(id));
+//   };
 
-  let content;
-  if (isLoading) {
-    content = <Spinner message="Ladowanie.." />;
-  } else if (newArr && newArr.length <= 0) {
-    content = (
-      <div className={classes.emptyCart}>
-        <h2>Twój koszyk jest pusty</h2>
-        <Link to="/sklep">
-          <button type="button">Wróć do sklepu</button>
-        </Link>
-      </div>
-    );
-  } else if (newArr) {
-    content = (
-      <CartProduct
-        products={newArr as []}
-        increaseHandler={increaseHandler}
-        decreaseHandler={decreaseHandler}
-      />
-    );
-  }
+//   let content;
+//   if (isLoading) {
+//     content = <Spinner message="Ladowanie.." />;
+//   } else if (newArr && newArr.length <= 0) {
+//     content = (
+//       <div className={classes.emptyCart}>
+//         <h2>Twój koszyk jest pusty</h2>
+//         <Link to="/sklep">
+//           <button type="button">Wróć do sklepu</button>
+//         </Link>
+//       </div>
+//     );
+//   } else if (newArr) {
+//     content = (
+//       <CartProduct
+//         products={newArr as []}
+//         increaseHandler={increaseHandler}
+//         decreaseHandler={decreaseHandler}
+//       />
+//     );
+//   }
 
-  return <div>{content}</div>;
-};
-export default NotRegisteredCart;
+//   return <div>{content}</div>;
+// };
+// export default NotRegisteredCart;
--- src/store/cartSlice.tsx
+++ src/store/cartSlice.tsx
@@ -1,66 +1,65 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { stat } from 'fs';
+// import { createSlice } from '@reduxjs/toolkit';
 
-interface CartState {
-  items: {
-    productId: string;
-    quantity: number;
-  }[];
-  totalQuantity: number;
-}
-const localStorageItems: CartState = localStorage.getItem('cartItems')
-  ? JSON.parse(localStorage.getItem('cartItems') as string)
-  : {
-      items: [],
-      totalQuantity: 0,
-    };
-const initialState: CartState = {
-  items: localStorageItems.items,
-  totalQuantity: localStorageItems.totalQuantity,
-};
+// interface CartState {
+//   items: {
+//     productId: string;
+//     quantity: number;
+//   }[];
+//   totalQuantity: number;
+// }
+// const localStorageItems: CartState = localStorage.getItem('cartItems')
+//   ? JSON.parse(localStorage.getItem('cartItems') as string)
+//   : {
+//       items: [],
+//       totalQuantity: 0,
+//     };
+// const initialState: CartState = {
+//   items: localStorageItems.items,
+//   totalQuantity: localStorageItems.totalQuantity,
+// };
 
-const cartItemsSlice = createSlice({
-  name: 'cartItems',
-  initialState,
-  reducers: {
-    onAddItem: (state, action) => {
-      const newItem = action.payload;
-      const existingItem = state.items.find(
-        (item) => item.productId === newItem.productId
-      );
-      if (!existingItem) {
-        state.items.push(newItem);
-      } else {
-        existingItem.quantity += newItem.quantity;
-      }
+// const cartItemsSlice = createSlice({
+//   name: 'cartItems',
+//   initialState,
+//   reducers: {
+//     onAddItem: (state, action) => {
+//       const newItem = action.payload;
+//       const existingItem = state.items.find(
+//         (item) => item.productId === newItem.productId
+//       );
+//       if (!existingItem) {
+//         state.items.push(newItem);
+//       } else {
+//         existingItem.quantity += newItem.quantity;
+//       }
 
-      // eslint-disable-next-line no-param-reassign
-      state.totalQuantity += newItem.quantity;
-    },
-    onIncreaseQty: (state, action) => {
-      const itemId = action.payload;
-      const existingItem = state.items.find(
-        (item) => item.productId === itemId
-      );
-      if (existingItem) {
-        existingItem.quantity += 1;
-        // eslint-disable-next-line no-param-reassign
-        state.totalQuantity += 1;
-      }
-    },
-    onDecreaseQty: (state, action) => {
-      const itemId = action.payload;
-      const existingItem = state.items.find(
-        (item) => item.productId === itemId
-      );
-      if (existingItem && existingItem.quantity > 1) {
-        existingItem.quantity -= 1;
-        // eslint-disable-next-line no-param-reassign
-        state.totalQuantity -= 1;
-      }
-    },
-  },
-});
+//       // eslint-disable-next-line no-param-reassign
+//       state.totalQuantity += newItem.quantity;
+//     },
+//     onIncreaseQty: (state, action) => {
+//       const itemId = action.payload;
+//       const existingItem = state.items.find(
+//         (item) => item.productId === itemId
+//       );
+//       if (existingItem) {
+//         existingItem.quantity += 1;
+//         // eslint-disable-next-line no-param-reassign
+//         state.totalQuantity += 1;
+//       }
+//     },
+//     onDecreaseQty: (state, action) => {
+//       const itemId = action.payload;
+//       const existingItem = state.items.find(
+//         (item) => item.productId === itemId
+//       );
+//       if (existingItem && existingItem.quantity > 1) {
+//         existingItem.quantity -= 1;
+//         // eslint-disable-next-line no-param-reassign
+//         state.totalQuantity -= 1;
+//       }
+//     },
+//   },
+// });
 
-export const cartItemAction = cartItemsSlice.actions;
-export default cartItemsSlice.reducer;
+// export const cartItemAction = cartItemsSlice.actions;
+// export default cartItemsSlice.reducer;
