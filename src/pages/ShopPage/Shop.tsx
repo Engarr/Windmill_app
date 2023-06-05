1026a03a429755293ccbfd59126c544d5b40fd01
Łukasz <lukasz.poczta12@gmail.com>
2023-06-05 14:58:57 +0200

    added sorting to Shop page

--- src/pages/ShopPage/Shop.tsx
+++ src/pages/ShopPage/Shop.tsx
@@ -15,6 +15,8 @@ const Shop = () => {
   const category = params.category || `Wszystkie produkty`;
   const [products, setProducts] = useState<Products[]>([]);
 
+  const [sortCriteria, setSortCriteria] = useState('recommended');
+
   // function for fetching all or category products
   const { data: allProductsArr, isLoading: isAllProductsLoading } =
     useGetAllProductsQuery(undefined, {
@@ -38,19 +40,41 @@ const Shop = () => {
     isAllProductsLoading,
     isCategoryProductsLoading,
   ]);
+
   let content;
 
   if (isAllProductsLoading || isCategoryProductsLoading) {
     content = <Spinner message="Wczytywanie produktów.." />;
   } else if (products.length > 0) {
+    let sortedProducts = [...products];
+
+    if (sortCriteria === 'recomended') {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      sortedProducts = products;
+    } else if (sortCriteria === 'alphabetically, A-Z') {
+      sortedProducts.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      });
+    } else if (sortCriteria === 'price ascending') {
+      sortedProducts.sort((a, b) => a.price - b.price);
+    } else if (sortCriteria === 'price descending') {
+      sortedProducts.sort((a, b) => b.price - a.price);
+    }
+
     content = (
       <>
-        {products.map((product) => {
+        {sortedProducts.map((product) => {
           return <Product key={product._id} product={product} />;
         })}
       </>
     );
-  } else if (products.length === 0) {
+  } else {
     content = (
       <Empty message="Nie posiadamy produktów w tej kategorii" width={300} />
     );
@@ -59,11 +83,31 @@ const Shop = () => {
   return (
     <section id="sklep">
       <div className={classes.navigation}>
-        <Link to="/">
-          <p>Strona główna</p>
-        </Link>
-        <span>/</span>
-        <p className={classes.navigation__active}>{category}</p>
+        <div className={classes.navigation__nav}>
+          <Link to="/">
+            <p>Strona główna</p>
+          </Link>
+          <span>/</span>
+          <p className={classes[`navigation__nav--active`]}>{category}</p>
+        </div>
+        <div className={classes.navigation__sorting}>
+          {/* <MdKeyboardArrowDown className={classes.arrow} /> */}
+          <label>Opcje sortowania:</label>
+          <select
+            name="sorting"
+            id="sorting"
+            value={sortCriteria}
+            onChange={(e) => setSortCriteria(e.target.value)}
+          >
+            <option value="recommended" className={classes.option}>
+              Domyślne sortowanie
+            </option>
+            <option value="alphabetically, A-Z">Alfabetycznie, A-Z</option>
+            <option value="alphabetically, Z-A">Alfabetycznie, Z-A</option>
+            <option value="price ascending">Cena rosnąco </option>
+            <option value="price descending">Cena malejąco</option>
+          </select>
+        </div>
       </div>
       <div className={classes.product__wrappper}>{content}</div>
     </section>
